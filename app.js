d1735d086d84cbfb13f53a7d616070dd636a5d18
Jordan Parker <parkerj4321@gmail.com>
2018-09-24 12:14:56 -0500

    first commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -18,24 +19,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'user2')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var path    = require("path");
+var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    console.log("Test!!!");
-    res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/architect', function(req, res, next) {
-    res.sendFile(path.join(__dirname+'/../public/architect.html'))
+router.get('/architect', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../public/architect.html'));
 });
 
-router.get('/art', function(req, res, next) {
-    res.sendFile(path.join(__dirname+'/../public/art.html'))
+router.get('/art', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../public/art.html'));
 });
 
-router.get('/band', function(req, res, next) {
-    res.sendFile(path.join(__dirname+'/../public/band.html'))
+router.get('/band', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../public/band.html'));
 });
-
-router.get('/food', function(req, res, next) {
-    res.sendFile(path.join(__dirname+'/../public/food.html'))
+router.get('/food', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../public/food.html'));
 });
-
 module.exports = router;
